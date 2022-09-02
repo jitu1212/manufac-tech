3518a1ed69c82531cb832c0fc4025b0210aed00f
jitu yadav <98046486+jitu1212@users.noreply.github.com>
2022-09-02 15:22:45 +0530

    Update BarChart.js

--- src/Component/BarChart/BarChart.js
+++ src/Component/BarChart/BarChart.js
@@ -2,59 +2,58 @@ import React from 'react'
 import ReactECharts from 'echarts-for-react'
 import {Data} from "../../Utills/WineData"
 
-
-
-  
-
-
-
-
 export default function BarChart() {
-let arr1=[];
-let arr2=[];
-let arr3=[];
-let arr=[];
 
 
+let avg=[]
+
+let temp=[];
 
 for(let i=0;i<Data.length;i++){
-
-  Data[i].Alcohol===1 ? arr1.push(Data[i].Malic_Acid) :arr.pop()
-  Data[i].Alcohol===2 ? arr2.push(Data[i].Malic_Acid) :arr.pop()
-  Data[i].Alcohol===3 ? arr3.push(Data[i].Malic_Acid) :arr.pop()
-
+  if(Data[i].Alcohol in temp){  
+  }
+  else{
+    temp.push([]);
+  }
 }
+Data.map((e)=>{
+ temp[e.Alcohol].push(e.Malic_Acid) 
+ 
+})
+console.log(temp)
 function getAvg(Values) {
   const total = Values.reduce((acc, c) => acc + c, 0);
   return total / Values.length;
 }
 
-const average1 = getAvg(arr1).toFixed(2);
-const average2= getAvg(arr2).toFixed(2);
-const average3= getAvg(arr3).toFixed(2);
+for(let i=1;i<temp.length;i++){
+  avg.push(getAvg(temp[i]).toFixed(2))
 
-
-
-
-
+}
+   const DataNames=temp.map((e,index)=>{
+    return(
+      `Alcohal-${index}`
+     
+        
+    )
+    }).splice(1)
+     
+     console.log(DataNames)
     const option = {
    
-        // title: {
-          
-        //   text: 'Average-content'
-        // },
+    
         tooltip: {},
         legend: {
           data:['Average-content']
         },
         xAxis: { name:"Alcohal",
-          data: ['Alcohal-1', 'Alcohal-2', 'Alcohal-3']
+          data: DataNames
         },
         yAxis: {name:"Malic_Acid"},
         series: [{
           name: 'Average-content',
           type: 'bar',
-          data: [average1, average2, average3]
+          data: avg
         }]
       };
   return (
